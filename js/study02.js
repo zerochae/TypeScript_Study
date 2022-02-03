81a6f7aeed01b9daa7f8f6a5a446b118cd103e86
zerochae <zerochae@kakao.com>
2022-02-03 11:50:56 +0900

    TypeScript Study02

---
+++ js/study02.js
@@ -0,0 +1,32 @@
+let 이름 = "kim";
+let 나이 = 20;
+let 결혼했니 = false;
+let 내정보 = { age: 20 };
+let 이름2 = "kim";
+let 널이니 = null;
+let 언디파인드 = undefined;
+let 이름3 = "kim";
+let 이름4 = 123;
+let 회원들 = ["kim", "park"];
+let 숫자들 = [123, 123];
+let 불불불 = [true, true];
+let 아무거나 = [1, "1", true];
+let 회원객체들 = {
+    member1: "kim",
+    member2: "park",
+};
+let 회원들2 = "park";
+let song = {
+    singer: "아이유",
+    title: "밤편지",
+};
+let zerochae = {
+    name: "권영채",
+    age: 100,
+    city: "대전",
+};
+let project = {
+    member: ["kim", "lee"],
+    days: 30,
+    started: false,
+};
---
+++ ts/study02.ts
@@ -0,0 +1,60 @@
+let 이름: string = "kim";
+
+let 나이: number = 20;
+let 결혼했니: boolean = false;
+
+let 내정보: { age: number } = { age: 20 };
+
+let 이름2: string = "kim";
+// 이름2 = 30;
+
+let 널이니: null = null;
+
+let 언디파인드: undefined = undefined;
+
+let 이름3: string = "kim";
+
+let 이름4: number = 123;
+
+let 회원들: string[] = ["kim", "park"];
+
+let 숫자들: number[] = [123, 123];
+
+let 불불불: boolean[] = [true, true];
+
+let 아무거나: any = [1, "1", true];
+
+let 회원객체들: { member1: string; member2: string } = {
+  member1: "kim",
+  member2: "park",
+};
+
+let 회원들2 = "park";
+
+let song: {
+  singer: string;
+  title: string;
+} = {
+  singer: "아이유",
+  title: "밤편지",
+};
+
+let zerochae: {
+  name: string;
+  age: number;
+  city: string;
+} = {
+  name: "권영채",
+  age: 100,
+  city: "대전",
+};
+
+let project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "lee"],
+  days: 30,
+  started: false,
+};
